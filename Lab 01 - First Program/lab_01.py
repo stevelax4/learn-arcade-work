5ad5bc91d43574310aeb9ffe5af4e5b7ef4af46f
stevelax4 <grossmans@hartwick.edu>
2025-03-25 22:29:36 -0400

    test

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,39 @@
-print("Hello World!\nHello, how is it going?\nI am great how are you?\nI am good.\nGreat!")
-print("What are you seeing?")
-print("A lot of people and lights!")
-print("Wonderful.")
+class Monster:
+    def __init__(self):
+        self.name = ""
+        self.health = 100
+
+    def decrease_health(self, lost_health):
+        self.health -= lost_health
+        print(self.name + "is now at " +  str(self.health) + " health ")
+        if self.health < 1:
+            print("The Monster Dies")
+
+
+def main():
+    gorp = Monster()
+    gorp.name = "Gorp"
+    gorp.decrease_health(100)
+
+main()
+
+class Cat:
+
+
+    def __init__(self):
+        self.color = ""
+        self.name = ""
+        self.weight = 0
+
+    def meow(self):
+        print(self.name + "  says \"meow\"")
+
+def main():
+    my_cat = Cat()
+    my_cat.name = "Buttercup"
+    my_cat.color = "orange"
+    my_cat.weight = 5
+    my_cat.meow()
+
+
+main()
