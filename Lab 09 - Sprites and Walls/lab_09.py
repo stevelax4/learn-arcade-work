bc969f06c541f2139b2b0baf3b42e4c3a07ad43a
stevelax4 <grossmans@hartwick.edu>
2025-04-13 18:53:57 -0400

    test

--- Lab 09 - Sprites and Walls/lab_09.py
+++ Lab 09 - Sprites and Walls/lab_09.py
@@ -3,6 +3,7 @@
 import arcade
 import random
 
+
 # --- Constants ---
 SPRITE_SCALING_BOX = 0.5
 SPRITE_SCALING_PLAYER = 0.5
@@ -26,11 +27,14 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = None
         self.wall_list = None
-        self.coin_list = 50
+        self.coin_list = None
 
         # Set up the player
         self.player_sprite = None
 
+        self.coin_sound = arcade.load_sound(":resources:sounds/coin5.wav")
+
+        self.score = 0
 
     def setup(self):
 
@@ -40,6 +44,7 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
 
         # Reset the score
         self.score = 0
@@ -55,28 +60,101 @@ class MyGame(arcade.Window):
         # Manually create and position a box at 300, 200
         wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
         wall.center_x = 200
-        wall.center_y = 172
+        wall.center_y = 244
         self.wall_list.append(wall)
 
         # Manually create and position a box at 364, 200
         wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
         wall.center_x = 328
-        wall.center_y = 172
+        wall.center_y = 180
         self.wall_list.append(wall)
 
+        # Manually create and position a box at 364, 200
+        wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
+        wall.center_x = 136
+        wall.center_y = 244
+        self.wall_list.append(wall)
+
+
         # --- Place boxes inside a loop
-        for x in range(200, 778, 64):
-            for i in range(2):
+        for x in range(200, 799, 64):
+            for i in range(1):
                 wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
                 wall.center_x = x
-                wall.center_y = i * 64 + 300
+                wall.center_y = i * 64 + 364
                 self.wall_list.append(wall)
+
+        for x in range(200, 630, 64):
+            for i in range(1):
+                wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
+                wall.center_x = x
+                wall.center_y = i * 64 + 308
+                self.wall_list.append(wall)
+
+        for x in range(1, 720, 64):
+            for i in range(1):
+                wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
+                wall.center_x = x
+                wall.center_y = i * 64 + 125
+                self.wall_list.append(wall)
+
+        for x in range(1, 799, 64):
+            for i in range(1):
+                wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
+                wall.center_x = x
+                wall.center_y = i * 64 + -8
+                self.wall_list.append(wall)
+
+        for x in range(1, 720, 64):
+            for i in range(1):
+                wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING_BOX)
+                wall.center_x = x
+                wall.center_y = i * 64 + 627
+                self.wall_list.append(wall)
+
+
+
 
         # --- Place walls with a list
         coordinate_list = [[400, 500],
                            [464, 500],
                            [400, 564],
-                           [464, 564]]
+                           [464, 564],
+                           [458, 180],
+                           [394, 180],
+                           [586, 244],
+                           [705, 180],
+                           [705, 244],
+                           [-3, 244],
+                           [-3, 180],
+                           [-3, 308],
+                           [-3, 372],
+                           [-3, 436],
+                           [-3, 500],
+                           [-3, 564],
+                           [60, 365],
+                           [125, 500],
+                           [189, 500],
+                           [189, 436],
+                           [253, 500],
+                           [253, 436],
+                           [585, 500],
+                           [585, 436],
+                           [699, 500],
+                           [699, 564],
+                           [820, 64],
+                           [820, 120],
+                           [820, 185],
+                           [820, 249],
+                           [820, 313],
+                           [820, 377],
+                           [820, 441],
+                           [820, 505],
+                           [820, 569],
+                           [820, 633],
+                           [820, 697],
+                           [820, 761],
+                           [820, 825]]
 
 
 
@@ -86,20 +164,39 @@ class MyGame(arcade.Window):
             wall.center_x = coordinate[0]
             wall.center_y = coordinate[1]
             self.wall_list.append(wall)
-            self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
-        #while len(self.coin_list) < COIN_COUNT:
-           # coin = arcade.Sprite(":resources:images/items/coinGold_ul.png", SPRITE_SCALING_COIN)
-           # coin.center_x = random.randrange(SCREEN_WIDTH)
-          #  coin.center_y = random.randrange(SCREEN_HEIGHT)
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+
+        while len(self.coin_list) < COIN_COUNT:
+            coin = arcade.Sprite(":resources:images/items/coinGold_ul.png", SPRITE_SCALING_COIN)
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+            hit_list = arcade.check_for_collision_with_list(coin, self.wall_list)
+            if len(hit_list) == 0:
+                self.coin_list.append(coin)
+
 
     def on_draw(self):
         arcade.start_render()
         self.player_list.draw()
         self.wall_list.draw()
+        self.coin_list.draw()
 
-    def update(self, delta_time):
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+    def update(self, delta_time, hit_list=None):
         self.physics_engine.update()
+
+        hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                        self.coin_list,)
+
+        for coin in hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+            arcade.play_sound(self.coin_sound)
+
+            self.coin_list.on_update(delta_time)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
